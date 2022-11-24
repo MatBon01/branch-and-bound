63da4c5119f1d13f712566710813e02f4b80c66d
Matteo Bongiovanni <mgb58@hotmail.co.uk>
2022-11-24 14:14:45 +0000

    Add notion of an ordering heuristic

---
+++ utils/search_tree_explorer/ordering_heuristic/ordering_heuristic.py
@@ -0,0 +1,10 @@
+from ...search_tree.search_tree_node import SearchTreeNode
+
+
+class OrderingHeuristic:
+    def __init__():
+        pass
+
+    # A lower value signifies a more promising node
+    def potential(self, node: SearchTreeNode) -> float:
+        pass
