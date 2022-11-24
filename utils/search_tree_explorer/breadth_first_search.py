be652bf6f464c399203ec816a379d5dd2de06e43
Matteo Bongiovanni <mgb58@hotmail.co.uk>
2022-11-24 10:10:36 +0000

    Add fathoming to breadth_first_search

--- utils/search_tree_explorer/breadth_first_search.py
+++ utils/search_tree_explorer/breadth_first_search.py
@@ -9,21 +9,43 @@ class BreadthFirstSearch(SearchTreeExplorer):
     def __init__(self):
         logging.info("Using BreadthFirstSearch Search Tree Explorer")
         self.order: queue.PriorityQueue[SearchTreeNode] = queue.PriorityQueue()
+        self.best_solution_time: int = -1
         self.ITERATIONS_INCREMENT: int = 1
 
     def put(self, node: SearchTreeNode) -> None:
-        self.order.put((node.level, node))
+        # Avoid putting in nodes that would have been fathomed
+        if self.best_solution_time == -1 or node.lower_bound < self.best_solution_time:
+            self.order.put((node.level, node))
 
     def next(self) -> tuple[SearchTreeNode, int]:  # second int is iterations increment
         node: SearchTreeNode
         _, node = self.order.get()
+        while (
+            self.best_solution_time != -1
+            and node.lower_bound >= self.best_solution_time
+        ):
+            _, node = self.order.get()
         return node, self.ITERATIONS_INCREMENT
 
     def finished(self) -> bool:
-        return self.order.empty()
+        if self.order.empty():
+            logging.debug("Finishing as no more possible nodes")
+            return True
+        level_node_pair: tuple[int, SearchTreeNode]
+        if all(
+            map(
+                lambda level_node_pair: level_node_pair[1].lower_bound
+                >= self.best_solution_time,
+                self.order.queue,
+            )
+        ):
+            logging.debug("Finishing as there are no better solutions")
+            return True
+        return False
 
     def fathom(self, solution_time: int) -> None:
-        pass
+        if self.best_solution_time == -1 or solution_time < self.best_solution_time:
+            self.best_solution_time = solution_time
 
     def best(self) -> SearchTreeNode:
         # Return the node at the highest level with the lowest bound
