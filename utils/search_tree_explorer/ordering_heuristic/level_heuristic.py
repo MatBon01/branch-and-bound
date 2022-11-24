84932dbbc2ba10476cb6f26556475bff709cbc82
Matteo Bongiovanni <mgb58@hotmail.co.uk>
2022-11-24 14:25:03 +0000

    Add a default level modifier

--- utils/search_tree_explorer/ordering_heuristic/level_heuristic.py
+++ utils/search_tree_explorer/ordering_heuristic/level_heuristic.py
@@ -1,3 +1,4 @@
+import math
 from typing import Callable
 
 from utils.search_tree.search_tree_node import SearchTreeNode
@@ -6,9 +7,21 @@ from .ordering_heuristic import OrderingHeuristic
 
 
 class LevelHeuristic(OrderingHeuristic):
-    def __init__(self, level_modifier: Callable[[int], float]):
+    def __init__(self, level_modifier: Callable[[int], float]=id):
         super().__init__()
         self.level_modifier = level_modifier
 
     def potential(self, node: SearchTreeNode) -> float:
         return self.level_modifier(node.level)
+
+
+def reciprocal(num: int) -> float:
+    return 1.0 / float(num)
+
+
+def id(num: int) -> float:
+    return float(num)
+
+
+def exponential_reciprocal(num: int) -> float:
+    return 1.0 / (math.e ** float(num))
